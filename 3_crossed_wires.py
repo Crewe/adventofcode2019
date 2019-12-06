189f60b00bea3817f2e8ed0cb4a6b12483d388f9
Ryan Edwards-Crewe <Crewe@users.noreply.github.com>
2019-12-06 17:55:46 -0500

    Update 3_crossed_wires.py
    Converted input to line segments

--- 3_crossed_wires.py
+++ 3_crossed_wires.py
@@ -1,6 +1,6 @@
 import aoc
 
-wire_paths = aoc.getInput("test.txt")
+wire_paths = aoc.getInput("input3.txt")
 wire_1 = aoc.csvToList(wire_paths[0])
 wire_2 = aoc.csvToList(wire_paths[1])
 
@@ -14,21 +14,33 @@ def findMaximums(wire):
         maximums[heading] += dist
     return maximums
 
-def findHorizontalSegments(wire):
-    seg = [(0,0,0,0)]
+def findLineSegments(wire):
+    seg = [[0,0,0,0]]
     index = 0
     for direction in wire:
         heading = direction[0]
-        previous = seg[index]
+        dist = int(direction[1:])
+        prev_point = (seg[index][0], seg[index][1])  #x0,y0
+        next_point = ()
+        if heading == 'L' or heading == 'D': dist *= -1
         if heading == 'L' or heading == 'R':
-            print('hey')
+            next_point = (prev_point[0] + dist, prev_point[1])
         elif heading == 'U' or heading == 'D':
-            print('yo')
+            next_point = (prev_point[0], prev_point[1] + dist)
+        seg[index][2] = next_point[0] #x1
+        seg[index][3] = next_point[1]  #y1
+        seg[index] = tuple(seg[index])
+        seg.append([next_point[0], next_point[1], 0, 0])
+        index += 1
+    seg.remove(seg[-1]) # remove point
+    return seg
 
-def findVerticalSegments(wire):
-   seg = [(0,0,0,0)]
+def findIntersections(wire_1, wire_2):
+    # Find horizontal wire_1 segments that cross wire_2 vertically
 
-MAX = 40000
-print(findMaximums(wire_1))
-print(findMaximums(wire_2))
-findHorizontalSegments(wire_1)
+    # Find vertical wire_1 segments that cross wire_2 horizontally
+
+for line in findLineSegments(wire_1):
+    print(line)
+for line in findLineSegments(wire_2):
+    print(line)
